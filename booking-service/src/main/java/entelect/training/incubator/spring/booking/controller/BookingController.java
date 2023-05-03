b0c640438b7e3531e468a9f67db9b0cd7b09e33e
Cuan Otto <cuan.otto@entelect.co.za>
2023-05-03 17:25:34 +0200

    Update booking servcie to project style maven module

--- booking-service/src/main/java/entelect/training/incubator/spring/booking/controller/BookingController.java
+++ booking-service/src/main/java/entelect/training/incubator/spring/booking/controller/BookingController.java
@@ -1,14 +1,9 @@
 package entelect.training.incubator.spring.booking.controller;
 
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("bookings")
 public class BookingController {
-
-    private final Logger LOGGER = LoggerFactory.getLogger(BookingController.class);
 }
--- booking-service/src/main/java/entelect/training/incubator/spring/booking/repository/BookingRepository.java
+++ booking-service/src/main/java/entelect/training/incubator/spring/booking/repository/BookingRepository.java
@@ -1,8 +1,9 @@
 package entelect.training.incubator.spring.booking.repository;
+
 import entelect.training.incubator.spring.booking.model.Booking;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookingRepository extends CrudRepository<Booking,Integer>{
+public interface BookingRepository extends CrudRepository<Booking,Integer> {
 }
