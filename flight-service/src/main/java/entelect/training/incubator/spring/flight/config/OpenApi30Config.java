2469a0830830373a5cd4b45412848362b6d1b8b9
Cuan Otto <cuan.otto@entelect.co.za>
2023-05-05 11:47:39 +0200

    Update docs

--- flight-service/src/main/java/entelect/training/incubator/spring/flight/config/OpenApi30Config.java
+++ flight-service/src/main/java/entelect/training/incubator/spring/flight/config/OpenApi30Config.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Booking Api", version = "v1"))
+@OpenAPIDefinition(info = @Info(title = "Flight Api", version = "v1"))
 @SecurityScheme(
         name = "basicAuth",
         type = SecuritySchemeType.HTTP,
